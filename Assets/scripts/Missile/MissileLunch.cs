da439656e2a4490bb3b4c345bd58546ca36f6b77
Yunhao Chen <peter.cyh@hotmail.com>
2014-11-22 23:32:55 +1100

    add missile

---
+++ Assets/scripts/Missile/MissileLunch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileLunch : MonoBehaviour {
+
+	public int speed = 300;
+	public GameObject target;
+
+	Vector3 direction;
+	// Use this for initialization
+	void Start () {
+		this.audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += ( transform.forward * speed * Time.deltaTime );
+	}
+}
---
+++ Assets/scripts/Plane/PlaneFire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneFire : MonoBehaviour {
+	public GameObject missile;
+	GameObject m;
+	public Transform missileSpawnPoint;
+	// Use this for initialization
+	void Start () {
+		missileSpawnPoint = this.transform.FindChild ("MissileLaunchPoint");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.G)) {
+
+			m = (GameObject)Instantiate(missile, missileSpawnPoint.position, missileSpawnPoint.rotation);
+
+		}
+	}
+}
